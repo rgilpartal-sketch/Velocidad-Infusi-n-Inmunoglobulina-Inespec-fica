677218c5ef1132e025305b7f9b0bf130734d1d37
rgilpartal-sketch <rgilpartal@gmail.com>
2026-01-18 19:01:58 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
+export default defineConfig({
+  base: '/Velocidad-Infusi-n-Inmunoglobulina-Inespec-fica/',
+  plugins: [react()],
+})
       server: {
         port: 3000,
         host: '0.0.0.0',
