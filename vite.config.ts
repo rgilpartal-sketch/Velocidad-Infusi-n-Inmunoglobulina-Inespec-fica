0cb86bf8910f55cc5341b215f1fdf35fb7aceae4
rgilpartal-sketch <rgilpartal@gmail.com>
2026-01-18 19:11:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
 import path from 'path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/Velocidad-Infusi-n-Inmunoglobulina-Inespec-fica/',
   plugins: [react()],
-})
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  define: {
+    'process.env.API_KEY': '""',
+    'process.env.GEMINI_API_KEY': '""'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
